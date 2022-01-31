ee341238159d810fcc476bd884d16b91f501c0f6
SinkLine_P <popovartem505@gmail.com>
2022-01-31 20:30:26 +0200

    add structure test

---
+++ HomeWorks/Lesson_11/src/test/java/ua/com/danit/ConsoleAppTest.java
@@ -0,0 +1,28 @@
+package ua.com.danit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConsoleAppTest {
+
+  @Test
+  public void fatherObj() {
+  }
+
+  @Test
+  public void motherObj() {
+  }
+
+  @Test
+  public void sendError() {
+  }
+
+  @Test
+  public void sendEditFamily() {
+  }
+
+  @Test
+  public void startApp() {
+  }
+}
