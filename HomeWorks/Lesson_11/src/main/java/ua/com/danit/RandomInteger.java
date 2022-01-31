ccdfc0285184103184d3086b38c8e0eaffffb201
SinkLine_P <popovartem505@gmail.com>
2022-01-31 16:29:59 +0200

    clear Random class

--- HomeWorks/Lesson_11/src/main/java/ua/com/danit/RandomInteger.java
+++ HomeWorks/Lesson_11/src/main/java/ua/com/danit/RandomInteger.java
@@ -1,36 +1,10 @@
 package ua.com.danit;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 class RandomInteger {
   public static int getRandomNumber() {
     int min = 1;
     int max = 10;
 
     return (int) ((Math.random() * (max - min)) + min);
-  }
-
-  public static String[] Cars;
-  public static String[] Wheels;
-
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-
-    System.out.print("enter count cars: ");
-    int countCars = scan.nextInt();
-
-    for (int c = 0; c < countCars; c++) {
-
-      Cars = new String[] {"cars " + (c + 1)};
-      System.out.println(Arrays.toString(Cars));
-      int rndInteger = getRandomNumber();
-
-      for (int w = 0; w < rndInteger; w++) {
-        Wheels = new String[] {Arrays.toString(Cars) + ", wheel count: " + rndInteger};
-        System.out.println(Arrays.toString(Wheels));
-      }
-
-    }
   }
 }
